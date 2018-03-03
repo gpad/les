ada55b6adef45b7fcdcdb95b3807595d4b5ab4d1
GPad <gpadovani@gmail.com>
2018-03-03 19:18:45 +0100

    Cart implementation (WIP)

--- lib/les/entities_supervisor.ex
+++ lib/les/entities_supervisor.ex
@@ -8,6 +8,16 @@ defmodule Les.EntitiesSupervisor do
     })
   end
 
+  def find_user(user_id) do
+    child_id = Les.UserEntity.get_entity_name(user_id)
+    children = Supervisor.which_children(__MODULE__)
+      |> Enum.filter(fn {id, _, _, _} -> id == child_id end)
+    case children do
+      [{_, pid, _, _}] -> {:ok, pid}
+      [] -> start_user(user_id)
+    end
+  end
+
   def start_link() do
     Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
--- lib/les/user_entity.ex
+++ lib/les/user_entity.ex
@@ -1,15 +1,12 @@
 defmodule Les.UserEntity do
   use GenServer
+  require Logger
 
   defmodule State do
     defstruct [:user]
   end
 
-  def get_entity_name(id), do: :"user_entity_#{id}"
-
-  def start_link(id) do
-    GenServer.start_link(__MODULE__, [id], name: get_entity_name(id))
-  end
+  def get_entity_name(user_id), do: :"user_entity_#{user_id}"
 
   def create(attrs) do
     with {:ok, user} <- Les.Accounts.create_user(attrs),
@@ -20,9 +17,11 @@ defmodule Les.UserEntity do
     end
   end
 
-  def init([id]) do
-    user = Les.Accounts.get_user!(id)
-    {:ok, %State{user: user}}
+  def find(user_id) do
+    case Les.EntitiesSupervisor.find_user(user_id) do
+      {:ok, pid} -> {:ok, pid}
+      _ -> {:error, :not_found}
+    end
   end
 
   def update(pid, attrs) do
@@ -31,6 +30,20 @@ defmodule Les.UserEntity do
 
   def get(pid) do
     GenServer.call(pid, {:get})
+  end
+
+  def add_product(pid, product_id, qty) do
+    GenServer.call(pid, {:add_product, product_id, qty})
+  end
+
+  def start_link(id) do
+    GenServer.start_link(__MODULE__, [id], name: get_entity_name(id))
+  end
+
+  def init([id]) do
+    Logger.info("Start process for user: #{id} - pid: #{inspect self()}")
+    user = Les.Accounts.get_user!(id)
+    {:ok, %State{user: user}}
   end
 
   def handle_call({:update, attrs}, _form, state) do
@@ -45,5 +58,12 @@ defmodule Les.UserEntity do
 
   def handle_call({:get}, _form, state) do
     {:reply, state.user, state}
+  end
+
+  def handle_call({:add_product, product_id, qty}, _form, state) do
+    {:ok, product} = Les.Accounts.Products.get(product_id)
+    {:ok, cart} = Les.Accounts.Cart.find_or_create(%{user_id: state.user.id})
+    {:ok, cart} = Les.Accounts.Cart.add_product(cart, product, qty)
+    {:reply, cart, state}
   end
 end
